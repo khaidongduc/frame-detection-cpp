6abd2fcde3756d6f9b746e2e2dbe5b15676d4433
Khai Dong <khaidongduc@gmail.com>
2022-06-18 11:18:12 -0400

    demo code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
+using namespace cv;
+using namespace std;
+
 const int DEFAULT_WIDTH = 500;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    VideoCapture cap("/home/khaidong/CLionProjects/frame-detection-cpp/samples/sample vid.mp4");
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream or file";
+    }
+    Mat frame;
+    while (cap.isOpened()) {
+        cap >> frame;
+        if (frame.empty())
+            break;
+        cvtColor(frame, frame, COLOR_BGR2GRAY); // convert to grayscale
+        imshow("frame", frame);
+        char c = (char) waitKey(25);
+        if (c == 27)
+            break;
+    }
+    cap.release();
+    destroyAllWindows();
     return 0;
 }
